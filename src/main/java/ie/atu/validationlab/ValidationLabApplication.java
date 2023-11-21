7535560e3f16cdbf85c43fea0d87b509211d9f35
G00400498@atu.ie <g00400498@atu.ie>
2023-11-21 15:33:23 +0000

    Created project and person class

---
+++ src/main/java/ie/atu/validationlab/ValidationLabApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.validationlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationLabApplication.class, args);
+    }
+
+}
